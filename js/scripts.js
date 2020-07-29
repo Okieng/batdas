e78d9f7db74a591c56d098b0bfde66fb5ce4df67
Okieng <yogisuk69@gmail.com>
2020-07-29 23:14:41 +0700

    header and section 1

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,18 @@ $(this).addClass("active");
 
 });
 
+            wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
